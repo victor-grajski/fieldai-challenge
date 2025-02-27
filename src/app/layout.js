bc36e85d850f0aaf43c3e0552baf48f9c92f6c48
Victor Grajski <victor.grajski@gmail.com>
2025-02-26 16:07:19 -0800

    test left dpad speed increase

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,23 @@
+'use client'
+
+import { useState } from "react";
 import "./globals.css";
 import DPad from '../components/DPad'
 
 export default function RootLayout({ children }) {
+  const [leftSpeed, setLeftSpeed] = useState(0)
+  const [rightSpeed, setRightSpeed] = useState(0)
+  const [upSpeed, setUpSpeed] = useState(0)
+  const [downSpeed, setDownSpeed] = useState(0)
+
   return (
     <html lang="en">
       <body className="container">
-        <DPad />
+        <p>Left: {leftSpeed}</p>
+        <p>Right: {rightSpeed}</p>
+        <p>Up: {upSpeed}</p>
+        <p>Down: {downSpeed}</p>
+        <DPad setLeftSpeed={setLeftSpeed} setRightSpeed={setRightSpeed} setUpSpeed={setUpSpeed} setDownSpeed={setDownSpeed} />
         {/* {children} */}
       </body>
     </html>
--- src/components/DPad.js
+++ src/components/DPad.js
@@ -1,9 +1,76 @@
+'use client'
 import styles from "./DPad.module.css";
+import { useEffect, useState } from "react";
 
-const DPad = () => {
+const DPad = ({ setLeftSpeed, setRightSpeed, setUpSpeed, setDownSpeed }) => {
+    const [leftClicked, setLeftClicked] = useState(false)
+
+    useEffect(() => {
+        if (leftClicked) {
+            const intervalId = setInterval(() => {
+                setLeftSpeed(prevSpeed => prevSpeed + 10)
+            }, 1000)
+
+            return () => clearInterval(intervalId)
+        } else {
+            setLeftSpeed(0)
+        }
+    }, [leftClicked])
+    
+    // every second, increase speed
+    // const handleLeft = (e) => {
+    //     e.preventDefault()
+
+    //     setInterval
+    // }
+
+    // const handleRight = (e) => {
+    //     e.preventDefault()
+
+    // }
+
+    // const handleUp = (e) => {
+    //     e.preventDefault()
+
+    // }
+
+    // const handleDown = (e) => {
+    //     e.preventDefault()
+
+    // }
+
     return (
         <div className={styles.container}>
-
+            <div 
+                className={styles.leftContainer} 
+                onTouchStart={() => setLeftClicked(true)}
+                onTouchEnd={() => setLeftClicked(false)}
+                onMouseDown={() => setLeftClicked(true)}
+                onMouseUp={() => setLeftClicked(false)}
+            >
+                Left
+            </div>
+            <div 
+                className={styles.rightContainer} 
+                onTouchStart={(e) => handleRight(e)}
+                // onClick={(e) => handleRight(e)}
+            >
+                Right
+            </div>
+            <div 
+                className={styles.upContainer} 
+                onTouchStart={(e) => handleUp(e)}
+                // onClick={(e) => handleUp(e)}
+            >
+                Up
+            </div>
+            <div 
+                className={styles.downContainer} 
+                onTouchStart={(e) => handleDown(e)}
+                // onClick={(e) => handleDown(e)}
+            >
+                Down
+            </div>
         </div>
     )
 }
